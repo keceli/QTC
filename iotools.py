de56882b24113723d094357b84120f5bcf93ace1
Murat Keceli <keceli@gmail.com>
2017-05-02 15:41:20 -0500

    Added pwd, check_dir

--- iotools.py
+++ iotools.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
+"""
+Contains IO related tools.
+Since python have different modules for various
+IO related functionalities, it is good to have
+a single module simplifying their usage.
+TODO: Add unit tests.
+"""
 import time
 import os
 from os.path import isfile
 
+__updated__ = "2017-05-02"
 
 def mkdir(path):
     """
@@ -10,12 +18,12 @@ def mkdir(path):
     Check http://stackoverflow.com/questions/273192/how-to-check-if-a-directory-exists-and-create-it-if-necessary
     """
     import os
-    try: 
+    try:
         os.makedirs(path)
     except OSError:
         if not os.path.isdir(path):
             raise
-    return    
+    return
 
 
 def cd(path):
@@ -27,20 +35,35 @@ def cd(path):
     return
 
 
-def write_file(s,filename='newfile'):
+def pwd():
+    """
+    Return current directory
+    """
+    import os
+    return os.getcwd()
+
+
+def join_path(*paths):
+    """
+    Concatenes strings into a portable path using correct seperators.
+    """
+    import os
+    return os.path.join(*paths)
+
+def write_file(s, filename='newfile'):
     """
     Writes s string to a file with the given'filename'.
     """
-    with open(filename,'w') as f:
+    with open(filename, 'w') as f:
         f.write(s)
     return
 
 
-def read_file(filename,aslines=True):
+def read_file(filename, aslines=False):
     """
     Reads a file and return either a list of lines or a string.
     """
-    with open(filename,'r') as f:
+    with open(filename, 'r') as f:
         if aslines:
             tmp = f.readlines()
         else:
@@ -66,7 +89,29 @@ def check_file(filename, timeout=0):
     return exists
 
 
+def check_dir(dirname, timeout=0):
+    """
+    Returns True (False) if a file exists (doesn't exist).
+    If timeout>0 is given, then checks file in a loop until
+    timeout seconds pass.
+    """
+    import time
+    from os.path import isdir
+    exists = isdir(dirname)
+    if timeout > 0 and not exists:
+        t0 = time.time()
+        while time.time() < t0 + timeout:
+            if isdir(dirname):
+                exists = True
+                break
+    return exists
+
+
 def check_exe(exename):
+    """
+    Check if an executable is available.
+    TODO:
+    """
     return True
 
 
@@ -76,24 +121,25 @@ def cp(source, target):
     Target can be a directory or a file.
     """
     from shutil import copy
-    copy(source,target)
+    copy(source, target)
     return
 
 
-def get_line_number(keyword,lines=None,filename=None):
+def get_line_number(keyword, lines=None, filename=None):
     """
     Returns the line number of a keyword found in given lines of string.
     Returns -1 if keyword is not found
     """
+    num = -1
     if lines is None and filename is None:
         print 'List of lines or a filename to be read is required for get_line_number'
-    elif filename:   
-        lines = read_file(filename,aslines=True)
+    elif filename:
+        lines = read_file(filename, aslines=True)
 
-    for no,line in enumerate(lines):
+    for n, line in enumerate(lines):
         if keyword in line:
-            return no
-    return -1
+            num = n
+    return num
 
 
 def read_list(listfile):
