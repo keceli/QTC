919ce7a09f5d84d96b44645cda153a69489660cf
Murat Keceli <keceli@gmail.com>
2017-05-02 15:39:30 -0500

    New command line options for running qc/thermo

--- thermo.py
+++ thermo.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 import argparse
-import numpy as np
-import datetime
-import time
 import subprocess
-import os
-from os.path import isfile
+
 import iotools as io
 import obtools as ob
 import qctools as qc
@@ -16,7 +12,20 @@ try:
     from scoop import utils
 except:
     runserial = True
-    print "scoop not available, no concurency"
+    print "No scoop, no concurency \n Running in serial mode..."
+
+__updated__ = "2017-05-02"
+_mopacexe = 'mopac'
+_nwchemexe = 'nwchem'
+_gaussianexe = 'mopac'
+_messpexe = 'messpf'
+_thermpexe = 'thermp'
+_pac99exe = 'pac99'
+_qcmethod = 'pm3'
+_qccode = 'mopac'
+_runqc = False
+_runthermo = False
+
 
 def get_args():
     """
@@ -28,28 +37,42 @@ def get_args():
     """
     April 18, 2017
     Murat Keceli
-    
+
     Performs quantum chemistry calculations to calculate thermochemical parameters.
     Writes NASA polynomials in different formats.
     Uses different codes for these purposes
     """)
-    parser.add_argument('-i','--input',type=argparse.FileType('r'),nargs=1,
+    parser.add_argument('-i', '--input', type=argparse.FileType('r'), nargs=1,
                         default='qc_list.txt',
                         help='List of inchi or smiles for species to be calculated')
-    parser.add_argument('-m','--messpf',type=str,nargs=1,
+    parser.add_argument('-m', '--qcmethod', type=str, nargs=1,
+                        default='pm3',
+                        help='Quantum chemistry method to be used')
+    parser.add_argument('-c', '--qccode', type=str, nargs=1,
+                        default='mopac',
+                        help='Quantum chemistry code to be used')
+    parser.add_argument('-q', '--runqc', action='store_true',
+                        help='Run quantum chemistry calculation')
+    parser.add_argument('-t', '--runthermo', action='store_true',
+                        help='Run thermochemistry calculations')
+    parser.add_argument('-r', '--runserial', action='store_true',
+                        help='Run serial')
+    parser.add_argument('--mopacexe', type=str, nargs=1,
+                        default='mopac',
+                        help='Path for mopac executable')
+    parser.add_argument('--messpf', type=str, nargs=1,
                         default='messpf',
-                        help='Path for mess partition function executable')
-    parser.add_argument('-t','--thermp',type=str,nargs=1,
+                        help='Path for MESS partition function executable')
+    parser.add_argument('--thermp', type=str, nargs=1,
                         default='thermp',
                         help='Path for thermp executable')
-    parser.add_argument('-p','--pac99',type=str,nargs=1,
+    parser.add_argument('--pac99', type=str, nargs=1,
                         default='pac99',
                         help='Path for pac99 executable')
-    parser.add_argument('-r','--runserial',action='store_true',
-                        help='Run serial')
     return parser.parse_args()
 
-def get_chemkin_polynomial(mol,method,zpe,xyz,freqs,deltaH):
+
+def get_chemkin_polynomial(mol, method, zpe, xyz, freqs, deltaH):
     """
     A driver to perform all operations to write NASA polynomial in
     chemkin format. Assumes quantum chemistry calculation is performed.
@@ -57,89 +80,81 @@ def get_chemkin_polynomial(mol,method,zpe,xyz,freqs,deltaH):
     inputfile = 'pf.inp'
     name = mol.formula
     tag = method
-    inp = tc.get_pf_input(mol, method, zpe, xyz,freqs)
-    
+    inp = tc.get_pf_input(mol, method, zpe, xyz, freqs)
+
  #   print 'Running mess partition function'
-    run_pf()
+    tc.run_pf()
  #   print 'Generate thermp input'
-    write_thermp_input(mol.formula, deltaH)
+    tc.write_thermp_input(mol.formula, deltaH)
  #   print 'Running thermp'
-    run_thermp()
+    tc.run_thermp()
 #    print 'Running pac99'
-    run_pac99(name)
+    tc.run_pac99(name)
 #    print 'Converting to chemkin format'
-    chemkinfile= name 
-    write_chemkin_file(deltaH, tag, name, name+'.ckin')
-    return 
+    chemkinfile = name + '.ckin'
+    tc.write_chemkin_file(deltaH, tag, name, chemkinfile)
+    return
 
-def run_pm3(s):
-    import os
+
+def run(s):
+    """
+    A driver function to run quantum chemistry and thermochemistry calculations based
+    on command line options:
+    --qcmethod
+    --qccode
+    """
     import qctools as qc
     import obtools as ob
-    method = 'pm3' 
+    import tctools as tc
+    import iotools as io
     mol = ob.get_mol(s)
-    natom = ob.get_natom(mol)
-    formula = mol.formula
-    hformula = qc.get_heavyatomformula(formula)
-    hlist    = qc.get_heavyatomlist(hformula)
-    key = ob.get_uniquekey(mol)
-    dir = 'database/' + hlist + '/' + hformula + '/' + formula + '/' + key + '/' + method
-    groupsfile='new.groups'
-    io.mkdir(dir)
-    if io.check_file(groupsfile):
-        if io.check_file(dir,1):
-            io.cp(groupsfile,dir)
-            io.cd(dir)
+#    natom = ob.get_natom(mol)
+#     if natom > 4:
+#         return 0
+    mult = ob.get_multiplicity(mol)
+    dirpath = ob.get_unique_path(mol, method=_qcmethod, mult=mult)
+    groupsfile = 'new.groups'
+    io.mkdir(dirpath)
+    cwd = io.pwd()
+    if _runthermo:
+        if io.check_file(groupsfile):
+            io.cp(groupsfile, dirpath)
+            if not io.check_file(groupsfile, 1):
+                print 'Could not copy new.groups file to target directory {0}'.format(dirpath)
+                return -1
         else:
-            print 'dir error', dir
+            print 'new.groups file required in working directory'
+            return -1
+    if io.check_dir(dirpath, 1):
+        io.cd(dirpath)
     else:
-        print 'groups file error'
-        return s    
-    lines = qc.run_mopac(mol=mol)
-    xyz = qc.get_xyz_mopac(lines)
-    freqs = qc.get_freq_mopac(lines)
-    zpe = qc.get_zpe_mopac(lines)
-    deltaH = qc.get_deltaH_mopac(lines)
-    return mol,method,zpe,xyz,freqs,deltaH
-
-
-def run_all(s):
-    import os
-    import qctools as qc
-    import obtools as ob
-    method = 'pm3' 
-    mol = ob.get_mol(s)
-    natom = ob.get_natom(mol)
-    formula = mol.formula
-    hformula = qc.get_heavyatomformula(formula)
-    hlist    = qc.get_heavyatomlist(hformula)
-    key = ob.get_uniquekey(mol)
-    dir = 'database/' + hlist + '/' + hformula + '/' + formula + '/' + key + '/' + method
-    groupsfile='new.groups'
-    io.mkdir(dir)
-    if io.check_file(groupsfile):
-        io.cp(groupsfile,dir)
-        io.cd(dir)
-    else:
-        print 'new.groups file required in working directory'
+        print 'I/O error, {0} directory not found'.format(dirpath)
         return -1
-    if not io.check_file(groupsfile,1):
-        print 'new.groups file required in target directory'
-        return -1    
-    lines = qc.run_mopac(mol=mol)
-    xyz = qc.get_xyz_mopac(lines)
-    freqs = qc.get_freq_mopac(lines)
-    zpe = qc.get_zpe_mopac(lines)
-    deltaH = qc.get_deltaH_mopac(lines)
-    get_chemkin_polynomial(mol,method,zpe,xyz,freqs,deltaH)
-    return 0    
-    
+    if _runqc:
+        if _qccode == 'mopac':
+            outfile = qc.run_mopac(s, mopacexe=_mopacexe, method=_qcmethod, mult=mult)
+            if _runthermo:
+                lines = io.read_file(outfile, aslines=True)
+                xyz = qc.get_mopac_xyz(lines)
+                freqs = qc.get_mopac_freq(lines)
+                zpe = qc.get_mopac_zpe(lines)
+                deltaH = qc.get_mopac_deltaH(lines)
+                get_chemkin_polynomial(mol, _qcmethod, zpe, xyz, freqs, deltaH)
+    io.cd(cwd)
+    return 0
+
+
 if __name__ == "__main__":
-    args   = get_args()
+    args = get_args()
+    print args
+    global _runqc
+    _runqc = args.runqc
+    _runthermo = args.runthermo
+    _qcmethod = args.qcmethod
+    _qccode = args.qccode
     mylist = io.read_list('qc_list.txt')
     if runserial:
-        returnValues = list(map(run_all,mylist))
+        returnValues = list(map(run, mylist))
     else:
-        returnValues = list(futures.map(run_all,mylist))
+        returnValues = list(futures.map(run, mylist))
 
-    
