0bba08abc67a0926679cd95669f5f27e90ddc1f3
Murat Keceli <keceli@gmail.com>
2017-06-23 15:07:57 -0500

    Added funcs for viewing, searching dictionary

--- dbtools.py
+++ dbtools.py
@@ -6,7 +6,7 @@ pymongo
 You need to start a mongo session first with:
 mongod --logpath /Volumes/s/G/database_mongo/log --fork --dbpath /Volumes/s/G/database_mongo/
 """
-__updated__ = "2017-06-02"
+__updated__ = "2017-06-23"
 
 from pymongo import MongoClient
 
@@ -32,6 +32,46 @@ def get_database(collection,db='mydatabase'):
 
 def insert_entry(entry,db,efilter={}):
     return db.replace_one(efilter,entry,upsert=True)
+
+def gen_dict_extract(key, var):
+    """
+    https://stackoverflow.com/questions/9807634/find-all-occurrences-of-a-key-in-nested-python-dictionaries-and-lists
+    """
+    if hasattr(var,'iteritems'):
+        for k, v in var.iteritems():
+            if k == key:
+                yield v
+            if isinstance(v, dict):
+                for result in gen_dict_extract(key, v):
+                    yield result
+            elif isinstance(v, list):
+                for d in v:
+                    for result in gen_dict_extract(key, d):
+                        yield result
+
+
+def visualise_dict(d,lvl=0):
+    """
+    https://stackoverflow.com/questions/15023333/simple-tool-library-to-visualize-huge-python-dict
+    """
+    # go through the dictionary alphabetically 
+    for k in sorted(d):
+        form = '{:<45} {:<15} {:<10}'
+        # print the table header if we're at the beginning
+        if lvl == 0 and k == sorted(d)[0]:
+            print(form.format('KEY','LEVEL','TYPE'))
+            print('-'*79)
+
+        indent = '  '*lvl # indent the table to visualise hierarchy
+        t = str(type(d[k]))
+
+        # print details of each entry
+        print(form.format(indent+str(k),lvl,t))
+
+        # if the entry is a dictionary
+        if type(d[k])==dict:
+            # visualise THAT dictionary with +1 indent
+            visualise_dict(d[k],lvl+1)
     
 if __name__ == "__main__":
     import doctest
