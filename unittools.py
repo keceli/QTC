4861bf775682710693ed60ba55dbc6da63e6c700
Murat Keceli <keceli@gmail.com>
2017-07-29 00:40:21 -0500

    Add unittols for unit conversions

---
+++ unittools.py
@@ -0,0 +1,9 @@
+#!usr/bin/python
+"""
+Contains constants for unit conversion
+TODO: Use IUPAC recommended values, calculate
+conversion constants from fundamental physical constants.
+"""
+
+rcm2au        = 4.556332E-6
+kjpermol2au   = 3.8088E-4
