9f9a4324a3c74e6ffd60291a0278d4d915d68904
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-09-07 13:39:23 -0500

    Top-level workflow runner

---
+++ swift/run-workflow.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu
+
+# RUN WORKFLOW
+# Run this to run the Swift/T workflow
+
+# Add Swift/T to PATH
+PATH=/home/wozniak/Public/sfw/blues/compute/swift-t/stc/bin:$PATH
+
+# Set up PYTHONPATH for QTC
+PYTHONPATH+=/home/keceli/.local/lib/python2.7/site-packages:
+PYTHONPATH+=/home/keceli/openbabel-2.4.1/install/lib/pys:
+PYTHONPATH+=/home/keceli/openbabel-2.4.1/install/lib/python2.7/site-packages
+
+# Run it!
+swift-t -l workflow.swift --input=test/syngas.txt |& tee workflow.out
