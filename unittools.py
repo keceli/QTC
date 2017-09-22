5c3db63fb75c03bcc41ffd28874018a903b61c84
Murat Keceli <keceli@gmail.com>
2017-09-22 10:38:18 -0500

    formatting

--- unittools.py
+++ unittools.py
@@ -6,5 +6,5 @@ conversion constants from fundamental physical constants.
 """
 
 rcm2au        = 4.556332E-6
-kj2au           = 3.8088E-4          # kj/mol to Hartree / atom
-kcal2kj         = 4.184                 # kcal/mol to kJ / mol
+kj2au         = 3.8088E-4          # kj/mol to Hartree / atom
+kcal2kj       = 4.184                 # kcal/mol to kJ / mol
