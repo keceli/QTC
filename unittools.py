9af32da2724e0a3e18c2f0f865bcdffbff5b51f5
Murat Keceli <keceli@gmail.com>
2017-09-22 22:17:46 -0500

    Added atomic masses

--- unittools.py
+++ unittools.py
@@ -5,6 +5,12 @@ TODO: Use IUPAC recommended values, calculate
 conversion constants from fundamental physical constants.
 """
 
-rcm2au        = 4.556332E-6
-kj2au         = 3.8088E-4          # kj/mol to Hartree / atom
-kcal2kj       = 4.184                 # kcal/mol to kJ / mol
+rcm2au   = 4.556332E-6
+kj2au    = 3.8088E-4          # kj/mol to Hartree / atom
+kcal2kj  = 4.184                 # kcal/mol to kJ / mol
+au2kcal  = 627.509608031
+#Atomic masses in a.u
+atommasses= {'H' : 1.00782503223,
+             'C' :12.00000000000,
+             'N' :14.00307400443,
+             'O' :15.99491461957}
