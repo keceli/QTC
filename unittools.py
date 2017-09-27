794e5d2f9b81e1e9d4038876af93b81ff7d5b010
Murat Keceli <keceli@gmail.com>
2017-09-27 16:19:41 -0500

    Added gas constant, more to do here

--- unittools.py
+++ unittools.py
@@ -9,6 +9,8 @@ rcm2au   = 4.556332E-6
 kj2au    = 3.8088E-4          # kj/mol to Hartree / atom
 kcal2kj  = 4.184                 # kcal/mol to kJ / mol
 au2kcal  = 627.509608031
+
+Rinkcal = 1.9872036#E-3 # gas constant in kcal/mol
 #Atomic masses in a.u
 atommasses= {'H' : 1.00782503223,
              'C' :12.00000000000,
