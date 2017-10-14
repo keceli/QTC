28796f79629d3dffbfd78ed10578e44ef0f09ea9
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-10-14 10:15:41 -0500

    Better variable name

--- swift/test/issue-5/workflow.swift
+++ swift/test/issue-5/workflow.swift
@@ -6,6 +6,6 @@ P = string2int(argv("P", "2"));
 
 foreach i in [0:3]
 {
-  A = [ int2string(i) ];
-  @par=P launch("./task.sh", A );
+  arguments = [ int2string(i) ];
+  @par=P launch("./task.sh", arguments);
 }
