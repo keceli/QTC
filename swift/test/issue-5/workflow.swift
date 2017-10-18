6e5568f32e4eb8f4249ed18e4be876b987583e27
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-10-18 10:04:40 -0500

    Demonstrate new hosts file functionality

--- swift/test/issue-5/workflow.swift
+++ swift/test/issue-5/workflow.swift
@@ -1,11 +1,14 @@
 
+import files;
 import launch;
 import sys;
 
 P = string2int(argv("P", "2"));
 
-foreach i in [0:3]
+foreach i in [0:0]
 {
   arguments = [ int2string(i) ];
-  @par=P launch("./task.sh", arguments);
+  file hosts = write("");
+  envs = [ "swift_write_hosts=%s" % filename(hosts) ];
+  @par=P launch_envs("./task.sh", arguments, envs);
 }
