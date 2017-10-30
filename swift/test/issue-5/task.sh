afc5765efa68bcadd9cd260f01c7e0a10464fda3
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-10-30 13:41:07 -0500

    Better error handling

--- swift/test/issue-5/task.sh
+++ swift/test/issue-5/task.sh
@@ -3,4 +3,4 @@
 ID=$1
 echo ID=$ID RANK=$PMI_RANK SIZE=$PMI_SIZE
 echo "[$ID]" "hosts file: ${swift_write_hosts}"
-cat ${swift_write_hosts} | sed "s/^/[$ID] /"
+cat ${swift_write_hosts:-NOTHING} | sed "s/^/[$ID] /"
