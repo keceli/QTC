5a2525b29f6743b891d2ba057a3738082bf3991c
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-10-30 13:41:38 -0500

    Save original shell task version for reference

---
+++ swift/test/issue-5/run-bebop-login-task.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+set -eu
+
+# Set P to argument 1 if given, else use default of 2
+P=${1:-2}
+
+module load intel/17.0.4-74uvhji
+module load jdk/8u141-b15-mopj6qr
+
+PATH=$HOME/Public/sfw/bebop/compute/swift-t/stc/bin:$PATH
+
+# MLS = MPI Launch for Swift
+export MLS=/home/wozniak/Public/sfw/bebop/mls/src
+
+swift-t -n 10 -I $MLS -r $MLS workflow-task.swift -P=$P
