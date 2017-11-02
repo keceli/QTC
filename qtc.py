96682707cc367f8f27c356b0c3234a2d424ca124
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-11-02 10:35:13 -0500

    Suggest not deleting these (ref #7)

--- qtc.py
+++ qtc.py
@@ -339,6 +339,7 @@ def run(s):
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             logging.error('Exception {}: {} {} {}'.format( e, exc_type, fname, exc_tb.tb_lineno))         
+            # Cf. #7
             io.rm(runfile)
             io.rm(qcoutput)
     if parseqc:
