6e10e87be80ec9c87251d2d5e053747ee9a1150c
snelliott <sarah.elliott26@uga.edu>
2018-07-24 18:22:34 -0500

    calc_bac and get_bac added to hf, fix for noninteger bond orders in cbh, small fixes

--- qtc/heatform.py
+++ qtc/heatform.py
@@ -301,11 +301,12 @@ def getname_fromdirname():
     cwd = os.getcwd()
     return cwd.split('/')[-1]
 
-def nest_2_dic(bas,key1,key2):
+def nest_2_dic(bas,key1,key2=0):
     """
     Returns a dictionary value that requires two key values (is in singly nested loop )
     """
-    from heatform_db import db
+    #from heatform_db import db
+    from newdb import db
  
     dicfound = False
     bas = ob.get_slabel(bas)
@@ -315,22 +316,27 @@ def nest_2_dic(bas,key1,key2):
             break
     if dicfound:
         if key1 in dic:
-            key2 = 'ANL1'
-            if key2 in dic[key1]:
-                logging.info('{} {}: {:.2f}  pulled from {} in heatform_db'.format(bas, key1, dic[key1][key2], key2))
-                return dic[key1][key2]
-            key2 = 'ATcTsig'
-            if key2 in dic[key1]:
-                if dic[key1][key2] < 1:
-                    if 'ATcT' in dic[key1]:
-                        logging.info('{} {}: {:.2f}  pulled from {} in heatform_db (sigma = {:.2f})'.format(bas, key1, dic[key1]['ATcT'], 'ATcT', dic[key1][key2]))
-                        return dic[key1]['ATcT']
-            key2 = 'ANL0'
-            if key2 in dic[key1]:
-                logging.info('{} {}: {:.2f}  pulled from {} in heatform_db'.format(bas, key1, dic[key1][key2], key2))
-                return dic[key1][key2]
+            if key2 == 0:
+                key2 = 'ANL1'
+                if key2 in dic[key1]:
+                    logging.info('{} {}: {:.2f}  pulled from {} in heatform_db'.format(bas, key1, dic[key1][key2], key2))
+                    return dic[key1][key2]
+                key2 = 'ATcTsig'
+                if key2 in dic[key1]:
+                    if dic[key1][key2] < 1:
+                        if 'ATcT' in dic[key1]:
+                            logging.info('{} {}: {:.2f}  pulled from {} in heatform_db (sigma = {:.2f})'.format(bas, key1, dic[key1]['ATcT'], 'ATcT', dic[key1][key2]))
+                            return dic[key1]['ATcT']
+                key2 = 'ANL0'
+                if key2 in dic[key1]:
+                    logging.info('{} {}: {:.2f}  pulled from {} in heatform_db'.format(bas, key1, dic[key1][key2], key2))
+                    return dic[key1][key2]
+            elif key2 == 'ANL0' or key2 == 'ANL1' or key2 == 'ATcT' or key2 == 'ATcTsig':
+                if key2 in dic[key1]:
+                    return dic[key1][key2]
+                else:
+                    return None
         logging.info('Value for ' + str(bas) + ',' + str(key1) + ' not found -- ommitting its contribution')
-
     return None
 
 def get_gaussian_zmat(filename):
@@ -860,28 +866,31 @@ def E_QTC(bas, opt, en, freq, parameters):
     qckeyword = parameters['qckeyword']
     qlabel = qc.get_qlabel(qckeyword, calcindex)
     en, zpve, bac = 0., 0., 0.
-
-    if 'energy' in parameters['all results'][slabel][qlabel]:
-        en = parameters['all results'][slabel][qlabel]['energy']
+    
+    if qlabel in parameters['all results'][slabel]:
+        if 'energy' in parameters['all results'][slabel][qlabel]:
+            en = parameters['all results'][slabel][qlabel]['energy']
     if en:
         logging.debug('Energy for {0} {1} = {2} Hartree'.format(slabel, qlabel,en))
     else: 
         logging.error('Energy not found for {0} {1}'.format(slabel, qlabel))
-    if 'azpve' in parameters['all results'][slabel][qlabel]:
-        zpve = parameters['all results'][slabel][qlabel]['azpve']
-        zpvelabel = 'anharmonic ' + qlabel
-    elif 'zpve' in parameters['all results'][slabel][qlabel]:
-        zpve = parameters['all results'][slabel][qlabel]['zpve']
-        zpvelabel = 'harmonic ' + qlabel
+    if qlabel in parameters['all results'][slabel]:
+        if 'azpve' in parameters['all results'][slabel][qlabel]:
+            zpve = parameters['all results'][slabel][qlabel]['azpve']
+            zpvelabel = 'anharmonic ' + qlabel
+        elif 'zpve' in parameters['all results'][slabel][qlabel]:
+            zpve = parameters['all results'][slabel][qlabel]['zpve']
+            zpvelabel = 'harmonic ' + qlabel
     else:
         for i in range(calcindex):
             qlabel = qc.get_qlabel(qckeyword, i)
-            if 'azpve' in parameters['all results'][slabel][qlabel]:
-                zpve = parameters['all results'][slabel][qlabel]['azpve']
-                zpvelabel = 'anharmonic ' + qlabel
-            elif 'zpve' in parameters['all results'][slabel][qlabel]:
-                zpve = parameters['all results'][slabel][qlabel]['zpve']
-                zpvelabel = 'harmonic ' + qlabel
+            if qlabel in parameters['all results'][slabel]:
+                if 'azpve' in parameters['all results'][slabel][qlabel]:
+                    zpve = parameters['all results'][slabel][qlabel]['azpve']
+                    zpvelabel = 'anharmonic ' + qlabel
+                elif 'zpve' in parameters['all results'][slabel][qlabel]:
+                    zpve = parameters['all results'][slabel][qlabel]['zpve']
+                    zpvelabel = 'harmonic ' + qlabel
     if zpve:
         logging.debug('ZPVE (harmonic) for {0} {1} = {2} Hartree'.format(slabel,zpvelabel,zpve))
     else: 
@@ -918,7 +927,7 @@ def main_keyword(s,parameters):
     molform = ob.get_formula(s)
    
     if 'cbh' in basis[0]:
-        clist, basis, basprint = cbh_coefficients(s.split('_')[0], basis[0])
+        clist, basis, basprint = cbh_coefficients(s.split('_')[0], basis[0], parameters)
     else:
         clist, basis, basprint = comp_coefficients(molform, basis)
  
@@ -945,7 +954,7 @@ def main_keyword(s,parameters):
     #parameters['suppress_printing']=False
     parameters['input']=s
     parameters['qckeyword'] = ','.join(qckeys)
-    return hf0k, basis
+    return hf0k, basis, clist
 
 def main(mol,logfile='',basis='auto',E=9999.9,optlevel='auto/',freqlevel='optlevel',enlevel='optlevel',anharm=False,runE=False, bas_not_smiles=False):
 
@@ -1111,7 +1120,6 @@ def get_adj_mat(lines):
             if atom.strip() in rad:
                 rads.append(i)
     atoms = qc.get_atom_list(rows[0])
-    
     mat = []
     for row in rows[1:]:
         if row:
@@ -1122,7 +1130,8 @@ def bond_sum(mat, atoms, heavy, i):
     bonds = 0
     for j, col in enumerate(mat[i]):
         if atoms[j] in heavy:
-            bonds += int(round(float(col)-0.5))
+            if float(col) > 0:
+                bonds += int(round(float(col)-0.5))
     return bonds
 
 def lhs_rhs(frags):
@@ -1173,7 +1182,7 @@ def CBHzed(smiles, mat, atoms, heavy, bond, rads):
     frags = make_balanced(smiles, frags)
     return frags
 
-def CBHone(smiles, mat, atoms, heavy, bond, rads):
+def CBHone(smiles, mat, atoms, heavy, bond, rads, parameters = {}):
     frags = {}
     valence = {'C':4, 'O':2, 'N':3}
     for i, row in enumerate(mat):
@@ -1186,8 +1195,8 @@ def CBHone(smiles, mat, atoms, heavy, bond, rads):
                        vali -= 1
                    if j in rads:
                        valj -= 1
-                   vali -= int(round(float(col)-0.5))
-                   valj -= int(round(float(col)-0.5))
+                   vali -= int(round(float(col)))
+                   valj -= int(round(float(col)))
                    fragi = atoms[i]
                    fragj = atoms[j]
                    saturate = ''
@@ -1202,13 +1211,35 @@ def CBHone(smiles, mat, atoms, heavy, bond, rads):
                    elif valj > 1:
                        saturate = 'H{:g}'.format(valj)
                    fragj = '[{}{}]'.format(fragj, saturate)
-                   frag = fragi + bond[col] + fragj
+                   frag = fragi + bond[str(int(round(float(col))))] + fragj
                    frag = ob.get_slabel(frag).split('_')[0]
                    if frag in frags:
                        frags[frag] += 1
                    else: 
                        frags[frag] = 1
-
+    #BALANCE
+    newfrags = {}
+    zedfrags = CBHzed(smiles, mat, atoms, heavy, bond, rads)
+    new = {}
+    frags =  {k: v for k, v in frags.items() if v}
+    for frag in frags:
+        newfrags[frag] = frags[frag]
+        atoms, mat, rads = get_x2zparams(frag, parameters)
+        new = CBHzed(frag, mat, atoms, heavy, bond, rads)
+        for n in new:
+           if n in newfrags:
+               newfrags[n] -= new[n] * frags[frag]
+           else:
+               newfrags[n] =- new[n] * frags[frag]
+    if not frags:
+        frags = CBHzed(smiles, mat, atoms, heavy, bond, rads)
+    for frag in zedfrags:
+        if frag in newfrags:
+            newfrags[frag] += zedfrags[frag]
+        else:
+            newfrags[frag]  = zedfrags[frag]
+    frags = newfrags
+    frags =  {k: v for k, v in frags.items() if v}
     frags = make_balanced(smiles, frags)
     return frags
 
@@ -1295,38 +1326,217 @@ def square_mat(mat):
                 fmat[i].append(mat[j][i])
     return fmat    
 
-def cbh_coefficients(smiles, ref):
-    xyz = ob.get_xyz(smiles)
+def get_recentxyz(s, results):
+    xyz = ''
+    slabel = ob.get_slabel(s)
+    if slabel in results:
+        for key in results:
+            if 'xyz' in results:
+                xyz = results[slabel]['xyz']
+    return xyz
+
+def choose_xyz(smiles, parameters):
+    xyz = ''
+    if 'all results' in parameters:
+        xyz = get_recentxyz(smiles, parameters['all results'])
+    if not xyz:
+        if 'xyzdir' in parameters:
+           xyz = io.read_xyzdir(smiles, parameters['xyzdir'])
+    if not xyz:
+            xyz = ob.get_xyz(smiles)
+    return xyz
+
+def get_x2zparams(smiles, parameters={}):
+    """
+    returns relevant x2z information for CBH calculation
+    INPUT: 
+    parameters -- dicitonary with qtc data (can be empty)
+    OUTPUT:
+    atoms      -- list of atoms in order of the x2z adjacency matrix
+    mat        -- triangular connectivity matrix from x2z
+    rads       -- list of radical sites zero indexed that correlate to the order of the atoms list
+    EXAMPLE:
+    >> get_x2zparams('C[C]=O',{})
+    ['C', 'C', 'O', 'H', 'H', 'H'], [[], ['1'], ['0', '2'], ['1', '0', '0'], ['1', '0', '0', '0'], ['1', '0', '0', '0', '0']],  [1]
+    """
+    xyz = choose_xyz(smiles, parameters)
     io.write_file(xyz,'x2z.xyz')
     lines = qc.run_x2z('x2z.xyz', 'x2z')
     atoms, mat, rads = get_adj_mat(lines)
- 
-    heavy = ['C','O','N']
-    bond  = {'1':'','2':'=','3':'#','4':'$','1.5':':'}
-    #CBH0
-    msg = ''
-    if ref.lower() == 'cbh0':
-        frags = CBHzed(smiles, mat, atoms, heavy, bond, rads)
-    elif ref.lower() == 'cbh1':
-        frags = CBHone(smiles, mat, atoms, heavy, bond, rads)
-    elif ref.lower() == 'cbh2':
-        frags = CBHtwo(smiles, mat, atoms, heavy, bond, rads)
+    return atoms, mat, rads
 
-    output = print_lhs_rhs(smiles, frags)
-    if isUnbalanced(get_balance(smiles, frags)):
-        output += '\nERROR: unbalanced fragmentation'
-    msg += output
-    msg += '\n'
+def cbh_coefficients(smiles, ref, parameters = {}):
+   
+    logging.info(smiles)
+    if ob.get_natom(smiles) < 2:
+        clist = [1]
+        fraglist = [smiles]
+        msg = 1
+    else:
+        atoms, mat, rads = get_x2zparams(smiles, parameters)
+        heavy = ['C','O','N']
+        bond  = {'1':'','2':'=','3':'#','4':'$','1.5':':','2.5':'=','1.7':'=','2.3':'=','1.3':'','2.7':'#'}
+        
+        msg = ''
+        if ref.lower() == 'cbh0':
+            frags = CBHzed(smiles, mat, atoms, heavy, bond, rads)
+        elif ref.lower() == 'cbh1':
+            frags = CBHone(smiles, mat, atoms, heavy, bond, rads, parameters)
+        elif ref.lower() == 'cbh2':
+            frags = CBHtwo(smiles, mat, atoms, heavy, bond, rads)
 
-    fraglist = []
-    clist = []
-    for frag in frags:
-        fraglist.append(frag)
-        clist.append(frags[frag])
-    print msg
+        output = print_lhs_rhs(smiles, frags)
+        if isUnbalanced(get_balance(smiles, frags)):
+            output += '\nERROR: unbalanced fragmentation'
+        msg += output
+        msg += '\n'
+
+        fraglist = []
+        clist = []
+        for frag in frags:
+            fraglist.append(frag)
+            clist.append(frags[frag])
     return clist, fraglist, msg
 
+def get_bac(parameters, mylist, samppercent = 0, errthresh = 10):
+    """
+    Computes BAC parameters (corrections to the HoF for each bond type for each level of theory)
+    INPUT
+    parameters  -- dictionary containing species info
+    mylist      -- list of species
+    samppercent -- percentage of species you want to form the set with (0 being all, 1 being none)
+    errthresh   -- threshold for what species will be ignored when forming BAC based on kj difference from ANL0 
+    OUTPUT
+    out         -- msg to log
+    bac.txt     -- BAC parameters file
+    """
+    out = 'begin least squares\n'
+    bondlabel = []
+    mols = []
+    taskmat = {}
+    testset = ''
+    bacset = ''
+    for i,s in enumerate(mylist):
+        anl = nest_2_dic(qc.get_slabel(s), 'delHf','ANL0')
+        bonds = None
+        if anl:
+            if np.random.rand() > samppercent:
+                sresults = parameters['all results'][qc.get_slabel(s)]
+                for qcresultkey, qcresultval in sorted(sresults.iteritems(),key= lambda x: x[0]):
+                    logging.debug(s)
+                    task = qcresultkey#qcresultval['chemkin'].splitlines()[0].split()[2]
+                    if task in taskmat:
+                        mat = taskmat[task]['mat']
+                        err = taskmat[task]['errors']
+                    else:
+                        mat = []
+                        err = []
+                        taskmat[task] = {}
+                        taskmat[task]['mat'] = mat
+                        taskmat[task]['errors'] = err
+                    energy = qcresultval['deltaH0']
+                    error = anl / ut.kcal2kj - energy
+                    #error = energy - anl/ ut.kcal2kj
+                    if abs(error) < errthresh:
+                        mols.append(qc.get_slabel(s))
+                        natom = ob.get_natom(ob.get_mol(s))
+                        if not bonds:
+                            if natom > 1:
+                                xyz = ''
+                                if 'xyz' in qcresultval:
+                                    xyz = qcresultval['xyz']
+                                if not xyz:
+                                    xyz = ob.get_xyz(ob.get_mol(s))
+                                x2zout = qc.run_x2z(xyz, parameters['x2z'])
+                                bacset += '{}\n'.format( qc.get_slabel(s)  )
+                                x2zout = qc.run_x2z(xyz, parameters['x2z'])
+                                bonds =  qc.get_x2z_bonds(x2zout)
+                            else:
+                                bonds = {}
+                            hofbasis = qcresultval['heat of formation basis']
+                            hofcoeff = qcresultval['heat of formation coeff']
+                            hofbonds = {}
+                            for c, bas in enumerate(hofbasis):
+                                nnatom = ob.get_natom(ob.get_mol(bas))
+                                if nnatom > 1:
+                                    xyz = ''
+                                    if 'xyz' in parameters['all results'][qc.get_slabel(bas)][task]:
+                                        xyz = parameters['all results'][qc.get_slabel(bas)][task]['xyz']
+                                    if not xyz:
+                                        xyz = ob.get_xyz(ob.get_mol(s))
+                                    x2zout = qc.run_x2z(xyz, parameters['x2z'])
+                                    hofbonds =  qc.get_x2z_bonds(x2zout)
+                                    for bond in hofbonds:
+                                        if bond in bonds:
+                                            bonds[bond] -= hofcoeff[c] * hofbonds[bond]
+                                        else:
+                                            bonds[bond] =- hofcoeff[c] * hofbonds[bond]
+                        bondarray = np.zeros(100)
+                        for bond in bonds:
+                            new = True
+                            j = 0
+                            for label in bondlabel:
+                                if bond == label:
+                                    bondarray[j] = bonds[bond]
+                                    new = False 
+                                    break
+                                j += 1
+                            if new:
+                                bondlabel.append(bond)
+                                bondarray[j] = bonds[bond]
+                        mat.append(bondarray.tolist())
+                        err.append(error)
+                        taskmat[task]['mat'] = mat
+                    else:
+                        out += 'ommitting {} from BAC, error is enormous\n'.format(s)
+                    taskmat[task]['errors'] = err
+            else:
+                testset += '{}\n'.format(qc.get_slabel(s))
+    io.write_file(bacset, 'formset.txt')
+    io.write_file(testset, 'testset.txt')
+    bacout = 'Formed from: {}\n'.format( ' ,'.join(mols)) 
+    for task in taskmat:
+        mat = taskmat[task]['mat']
+        mat = [mat[k][:len(bondlabel)] for k in range(len(mat))]
+        errors =  taskmat[task]['errors']
+        findbac = True
+        try:
+            #print task
+            #print 'AVG \t {:.4f} \nRMS \t {:.4f}'.format(np.average(errors),np.sqrt(np.average(np.square(errors))))
+            coeff = np.linalg.lstsq(mat, errors)[0]
+            bacout +=  '\n' + task + '\n'
+            for k, label in enumerate(bondlabel):
+                bacout += '{}  {:4.3f}\n'.format(label, coeff[k])
+            out += 'BAC parameters successfully computed for {}, appended to bac.txt\n'.format(task)
+        except:
+           out += 'Cannot compute BAC for {}, nan found in errors\n'.format(task)
+    io.write_file(bacout, 'strictestbac.txt')
+    return out
       
+def calc_bac(parameters, bonds, task):
+    """
+    Given a set of bonds, calculate the bond additivity correction
+    using a  template specified in parameters
+    """
+    correction = 0.
+    bacfile = parameters['bacdirectory'] +'/bac.txt'
+    #bacfile = 'bac.txt'
+    bacfile = io.read_file(bacfile)
+    if task + '\n' in bacfile:
+        bacfile = bacfile.split(task + '\n')[1].split('\n\n')[0]
+        bacfile = bacfile.splitlines()
+        bacdic  = {}
+        for line in bacfile:
+            line = line.split()
+            if len(line) > 1:
+                bacdic[line[0]] = float(line[1])
+        for key in bonds:
+            if key in bacdic:
+                correction += bonds[key] * bacdic[key] 
+            else:
+                logging.info( '{} bond has no correction'.format(key))
+    return correction
+
 if __name__ == '__main__': 
     """
     Run heat of formation code
