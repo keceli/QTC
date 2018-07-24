4f4e1633101dd3c575f7ec72b1af2ba19de64600
snelliott <sarah.elliott26@uga.edu>
2018-07-24 18:47:58 -0500

    more units

--- qtc/unittools.py
+++ qtc/unittools.py
@@ -11,7 +11,6 @@ kcal2kj  = 4.184                 # kcal/mol to kJ / mol
 au2kcal  = 627.509608031
 ev2au    = 0.03674930495
 Rinkcal = 1.9872036#E-3 # gas constant in cal/mol
-ghz2cm   = 0.0333564 
 #Atomic masses in a.u
 atommasses= {'H' : 1.00782503223,
              'C' :12.00000000000,
