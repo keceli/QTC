cfc377c277e46b4b88717ff158c9359659ca856c
snelliott <sarah.elliott26@uga.edu>
2018-07-30 14:34:48 -0500

    resolved merge conflict

--- qtc/tctools.py
+++ qtc/tctools.py
@@ -657,7 +657,6 @@ def get_messpf_input(mol,parameters):
             for i in range( len(xmat)):
                 inp += '\t\t' + ' '.join([str(xmat[i][j]) for j in range(i+1)]) + '\n'
         if not coreIsMd:
-<<<<<<< HEAD
             if 'norot' in parameters:
                 if not parameters['norot']:
                     #if len(rotconsts) > 0:
@@ -674,23 +673,6 @@ def get_messpf_input(mol,parameters):
                         inp += '      RotationalDistortion[1/cm]\n'
                         inp += '\t   ' + '\t   '.join(rotdists.splitlines(True)) + '\n'
                         inp += '      End\n' ###   END CORE
-=======
-            if not parameters['norot']:
-                #if len(rotconsts) > 0:
-                #    inp += '      RotationalConstants[1/cm] '
-                #    inp += ' '.join(rotconsts) + '\n'
-                if vibrots:
-                    vibrots = vibrots.splitlines(True)
-                    if len(freqs) == len(vibrots):
-                        inp += '      RovibrationalCouplings[1/cm]\n'
-                        inp += '\t   ' + '\t   '.join(vibrots) + '\n'
-                    else:
-                        logging.warning("Rotational Couplings length does not match freqs -- removed from pf.inp")
-                if len(rotdists) > 0:
-                    inp += '      RotationalDistortion[1/cm]\n'
-                    inp += '\t   ' + '\t   '.join(rotdists.splitlines(True)) + '\n'
-                    inp += '      End\n' ###   END CORE
->>>>>>> master
             inp += hindlines  ###   END RRHO
         if not 'hindered potential' in results:
             inp += '   End\n'
